1eda60e17c9d33d4143f048a138fff215dd19818
Thiago Paim <thiagopaim@msn.com>
2023-04-13 01:30:12 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>Opa</p>
+  </div>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    coverage: {
+      provider: 'istanbul' // or 'c8'
+    },
+  },
+  plugins: [vue()],
+})
